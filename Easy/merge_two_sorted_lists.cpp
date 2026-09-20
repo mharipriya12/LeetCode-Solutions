3b1637f888a2c07b5dcb11d616ad6afddc226b17
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 15:09:57 +0530

    Add mergeTwoLists function and printList helper
    Implement a function to merge two sorted linked lists and a helper function to print the list.

---
+++ Easy/merge_two_sorted_lists.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode dummy(0);
+    ListNode* current = &dummy;
+
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->val <= list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+
+        current = current->next;
+    }
+
+    if (list1 != nullptr)
+        current->next = list1;
+    else
+        current->next = list2;
+
+    return dummy.next;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(3);
+    list1->next->next = new ListNode(5);
+
+    ListNode* list2 = new ListNode(2);
+    list2->next = new ListNode(4);
+    list2->next->next = new ListNode(6);
+
+    ListNode* result = mergeTwoLists(list1, list2);
+
+    printList(result);
+
+    return 0;
+}
