e4dd7f6f775cc759cff13d70876b57c4a187e03d
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 15:04:29 +0530

    Implement valid parentheses checker in C++

---
+++ Easy/valid_parentheses.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        else {
+            if (st.empty())
+                return false;
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '['))
+                return false;
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+    string s = "()[]{}";
+
+    cout << (isValid(s) ? "true" : "false");
+
+    return 0;
+}
