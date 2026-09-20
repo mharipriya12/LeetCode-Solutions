bad66a384479e81009e652fc307c80ba433f4fd2
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 15:43:31 +0530

    Add maximum product subarray solution in C++
    Implement function to find maximum product of subarray.

---
+++ Medium/maximum_product_subarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+    int currentMax = nums[0];
+    int currentMin = nums[0];
+    int answer = nums[0];
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] < 0)
+            swap(currentMax, currentMin);
+
+        currentMax = max(nums[i], currentMax * nums[i]);
+        currentMin = min(nums[i], currentMin * nums[i]);
+
+        answer = max(answer, currentMax);
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> nums = {2, 3, -2, 4};
+
+    cout << maxProduct(nums);
+
+    return 0;
+}
