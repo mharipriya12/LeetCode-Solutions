cd5dacb554ce6428ab2d4317f53710217a7743c9
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 15:47:10 +0530

    Add topKFrequent function to find top k elements
    Implement a function to find the top k frequent elements in a vector.

---
+++ Medium/top_k_frequent_elements.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int, int> frequency;
+
+    for (int num : nums)
+        frequency[num]++;
+
+    priority_queue<pair<int, int>,
+                   vector<pair<int, int>>,
+                   greater<pair<int, int>>> minHeap;
+
+    for (auto item : frequency) {
+        minHeap.push({item.second, item.first});
+
+        if (minHeap.size() > k)
+            minHeap.pop();
+    }
+
+    vector<int> result;
+
+    while (!minHeap.empty()) {
+        result.push_back(minHeap.top().second);
+        minHeap.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1, 1, 1, 2, 2, 3};
+    int k = 2;
+
+    vector<int> result = topKFrequent(nums, k);
+
+    for (int num : result)
+        cout << num << " ";
+
+    return 0;
+}
